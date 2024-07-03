d3e80c97f3f3b5bd23068ea0d208f6365ad4900e
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-07-03 18:52:17 +0530

    update

--- src/components/tcarousel.tsx
+++ src/components/tcarousel.tsx
@@ -57,9 +57,9 @@ export default function Carousel() {
     useEffect(() => {
         client.getEntries({
             content_type: 'testimonial'
-        }).then((entries) => {
+        }).then((entries:any) => {
             let tmp: data[] = []
-            entries.items.forEach((e) => {
+            entries.items.forEach((e:any) => {
                 let entry = JSON.parse(JSON.stringify(e));
                 let itm: data = { img:'', des: '',name:'',relation:'',category:''};
                 if (entry.fields) {
