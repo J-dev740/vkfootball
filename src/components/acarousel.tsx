7a003df977fca872518a7301e510d2df0c788073
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-07-04 07:49:26 +0530

    updated centers carousel with cms data

--- src/components/acarousel.tsx
+++ src/components/acarousel.tsx
@@ -4,6 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdLocationPin } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { client } from "./client";
 
 const settings = {
     dots: false,
@@ -45,25 +47,62 @@ const settings = {
 
 
 }
+interface data{
+    img:string,
+    pos:string,
+    name:string,
+    contact:string,
+}
 export default function Carousel() {
+
+    const [data, setData] = useState<data[]>([])
+    useEffect(() => {
+        client.getEntries({
+            content_type: 'centers'
+        }).then((entries:any) => {
+            let tmp: data[] = []
+            entries.items.forEach((e:any) => {
+                let entry = JSON.parse(JSON.stringify(e));
+                let itm: data = { img:'', pos: '',name:'',contact:''};
+                if (entry.fields) {
+                    entry=entry.fields;
+                    
+                    // console.log("entry",entry)
+                    itm.img = entry.centerImage.fields.file.url;
+                    itm.pos=entry.location;
+                    itm.name=entry.name
+                    itm.contact=entry.contact
+                }
+                // console.log(itm)
+                tmp.push(itm);
+            })
+            return tmp;
+        }).then((res: data[]) => {
+            setData(res);
+        })
+
+    }, [])
+    useEffect(() => {
+        console.log(data);
+    }, [data])
     return (
         <Slider className="flex relative max-w-full max-h-full gap-[20px]  w-[90%] lg:w-[68%]  h-[500px] lg:h-[457px] flex-row  items-start justify-center "
             {...settings}
         >
-            {coachNames.map((v,idx)=>(
-                <Card idx={idx} coach={v}/>
+            {data.map((v:data)=>(
+                <Card  center={v}/>
             ))}
         </Slider>
 
     )
 }
 
-const Card=({idx,coach}:{idx:any,coach:any})=>{
+const Card=({center}:{center:data})=>{
     return (
         <div className=" relative flex w-[307px] bg-white  h-fit flex-col items-start  gap-[16px] ">
             {/* first div */}
             <div 
-            style={{backgroundImage:`url(/center${(idx%3)+1}.png)`}}
+            style={{backgroundImage:`url(https:${center.img})`}}
             className="flex flex-row items-end justify-start w-[353px] h-[479px] bg-cover  bg-top bg-no-repeat  gap-[8px] ">
             <div className="flex w-full h-full bg-black bg-opacity-40">
 
@@ -73,15 +112,15 @@ const Card=({idx,coach}:{idx:any,coach:any})=>{
             <div className="absolute bottom-0 flex flex-col items-start justify-start gap-[12px]  p-4 text-wrap">
               <div className=" flex flex-col items-start justify-start gap-[10px] text-wrap">
                 {/* header */}
-                <span className=" text-white text-[24px] font-semibold  leading-[28px] ">{coach.name}</span>
+                <span className=" text-white text-[24px] font-semibold  leading-[28px] ">{center.name}</span>
                 {/* description */}
                     <div className="flex flex-row gap-[12px] items-center justify-start ">
                         <div className="flex w-[20px] h-[20px]  text-[18px] text-white bg-transparent "><MdLocationPin /></div>
-                        <span className="text-white  text-[16px] leading-[18px] font-normal">{coach.pos}</span>
+                        <span className="text-white  text-[16px] leading-[18px] font-normal">{center.pos}</span>
                     </div>
                     <div className="flex flex-row gap-[12px] items-center justify-start ">
                         <div className="flex w-[20px] h-[20px]  text-[18px] text-white bg-transparent "><FaPhoneAlt /></div>
-                        <span className="text-white  text-[16px] leading-[18px] font-normal">+91 7977418993</span>
+                        <span className="text-white  text-[16px] leading-[18px] font-normal">{center.contact}</span>
                     </div>
 
               </div>
@@ -93,20 +132,20 @@ const Card=({idx,coach}:{idx:any,coach:any})=>{
         </div>
     )
 }
-const coachNames=[
-    {
-        name:"Rustomjee Urbania ",
-        pos:"Majiwada, Thane West, Mumbai",
-        phone:"+91 7977418993"
-    },
-    {
-        name:"Lodha Luxuria",
-        pos:"Majiwada, Thane West, Mumbai",
-        phone:"+91 7977418993"
-    },
-    {
-        name:"Hiranandani Meadows",
-        pos:"Majiwada, Thane West, Mumbai",
-        phone:"+91 7977418993"
-    },
-]
+// const coachNames=[
+//     {
+//         name:"Rustomjee Urbania ",
+//         pos:"Majiwada, Thane West, Mumbai",
+//         phone:"+91 7977418993"
+//     },
+//     {
+//         name:"Lodha Luxuria",
+//         pos:"Majiwada, Thane West, Mumbai",
+//         phone:"+91 7977418993"
+//     },
+//     {
+//         name:"Hiranandani Meadows",
+//         pos:"Majiwada, Thane West, Mumbai",
+//         phone:"+91 7977418993"
+//     },
+// ]
